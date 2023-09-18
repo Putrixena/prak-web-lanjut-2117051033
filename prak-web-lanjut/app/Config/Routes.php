bc3f0a3a520c55500c6aa70870a029c7d0f0a7f1
putrixenaaa <putrimm981@gmail.com>
2023-09-18 22:04:47 +0700

    update form-controller

--- prak-web-lanjut/app/Config/Routes.php
+++ prak-web-lanjut/app/Config/Routes.php
@@ -1,8 +1,25 @@
 <?php
 
+use App\Controllers\Home;
+use App\Controllers\Pages;
+use App\Controllers\UserController;
 use CodeIgniter\Router\RouteCollection;
 
 /**
  * @var RouteCollection $routes
  */
+
 $routes->get('/', 'Home::index');
+// mengarahkan jika diketik profile maka diarahkan ke fungsi profile
+$routes->get('/profile', 'Home::profile');
+$routes->get('/profile/(:any)/(:any)/(:any)', [Home::class, 'nama']);
+
+$routes->get('/user/profile', [UserController::class, 'profile']);
+$routes->get('/user/create', [UserController::class, 'create']);
+$routes->post('/user/store', [UserController::class, 'store']);
+
+
+
+$routes->get('/profile/(:any)', [Home::class, 'profile']);
+
+$routes->get('/pages', [Pages::class, 'index']);
